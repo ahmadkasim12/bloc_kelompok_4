b979a9c71daeb0f5f76fedfd2090293bd83ca030
Jii311 <jihadfadhilahakbar@gmail.com>
2024-09-03 15:57:33 +0700

    [JIhat] Initial Commit

---
+++ lib/bloc/bloc_bangun_ruang/bola.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc_aritmatika/input_bloc.dart';
+
+class KalkulasiBola extends Bloc<InputEvent, double> {
+  KalkulasiBola({this.operatorResult = 0}) : super(operatorResult);
+  double operatorResult;
+
+  void doOperation(double input5) {
+    emit(operatorResult = 4 / 3 * 3.14 * (input5 * input5 * input5));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,26 +3,33 @@ import 'package:bloc_implementation_assignment/Screens/BangunDatarScreen.dart';
 import 'package:bloc_implementation_assignment/Screens/BangunRuangScreen.dart';
 import 'package:bloc_implementation_assignment/Screens/Perpangkatan.dart';
 import 'package:bloc_implementation_assignment/bloc/bloc_aritmatika/input_bloc.dart';
+import 'package:bloc_implementation_assignment/bloc/bloc_bangun_ruang/input-ruang.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(
+  runApp(MultiBlocProvider(
+    providers: [
       BlocProvider(
         create: (context) => InputHolderBloc(),
-        child: MaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-            useMaterial3: true,
-          ),
-          initialRoute: '/arimatika',
-          routes: {
-            '/arimatika': (context) => Aritmatikapage(),
-            '/bangundatar': (context) => Bangundatarscreen(),
-            '/bangunruang': (context) => Bangunruangscreen(),
-            '/perpangkatan': (context) => Perpangkatan(),
-          },
-        ),
-      ));
+      ),
+      BlocProvider(
+        create: (context) => InputHolderBloc2(),
+      )
+    ],
+    child: MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      initialRoute: '/arimatika',
+      routes: {
+        '/arimatika': (context) => Aritmatikapage(),
+        '/bangundatar': (context) => Bangundatarscreen(),
+        '/bangunruang': (context) => Bangunruangscreen(),
+        '/perpangkatan': (context) => Perpangkatan(),
+      },
+    ),
+  ));
 }
